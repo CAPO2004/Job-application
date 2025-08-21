c94c026ad2d7969b949a591f9d89e7bed94a8787
CAPO2004 <capoca2004@gmail.com>
2025-08-21 22:23:38 +0300

    Add files via upload

---
+++ script (6).js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const toggleButton = document.getElementById('toggleMode');
+  const body = document.body;
+
+  toggleButton.addEventListener('click', () => {
+    if (body.classList.contains('light')) {
+      body.classList.remove('light');
+      body.classList.add('dark');
+      toggleButton.textContent = 'الوضع النهاري';
+    } else {
+      body.classList.remove('dark');
+      body.classList.add('light');
+      toggleButton.textContent = 'الوضع الليلي';
+    }
+  });
+});
